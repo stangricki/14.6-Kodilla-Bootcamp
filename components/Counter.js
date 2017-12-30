45173bb3a3ce01b66e0956da4f71446b1d90b148
Filip Stangricki <stangricki@gmail.com>
2017-12-30 18:45:06 +0100

    Added console logs

--- components/Counter.js
+++ components/Counter.js
@@ -2,12 +2,22 @@
 //"dodaj do komponentu Counter wszystkie metody cyklu życia komponentu. W każdej z nich wypisz console.log ze swoimi pomysłami na ich użycie"
 
 var Counter = React.createClass({
-
+    getDefaultProps: function() {
+        console.log('Getting default props...');
+        return {counter: 0};
+    },
     getInitialState: function() {
         return {
             counter: 0
         };
     },
+componentWillMount: function() {
+        console.log('I\'m about to mount...');
+        return {
+            counter: 0
+        };
+    },
+
 
     increment: function() {
         this.setState({
